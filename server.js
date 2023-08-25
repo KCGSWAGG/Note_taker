83f362d5d5974624cb6098887ef79036825f08bd
KCGSWAGG <Tyrealworley999@yahoo.com>
2023-08-24 21:53:11 -0400

    added route handling for different pages

--- server.js
+++ server.js
@@ -14,3 +14,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // API routes
 app.use('/api', apiRoutes);
+
+// Route handlers for different pages
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'))
+);
