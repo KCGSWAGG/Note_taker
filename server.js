c49597d05ffddbdcbacb984967f5b2ef2d116d5f
KCGSWAGG <Tyrealworley999@yahoo.com>
2023-08-24 21:54:17 -0400

    wildcard route and direct users to index.html

--- server.js
+++ server.js
@@ -23,3 +23,12 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public', 'notes.html'))
 );
+
+// Wildcard route to handle 404 and direct users to index.html
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
