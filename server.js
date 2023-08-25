09f4c7f6df8a44a85e836877580738ddad1e4663
KCGSWAGG <Tyrealworley999@yahoo.com>
2023-08-24 21:52:36 -0400

    added static files and API routes

--- server.js
+++ server.js
@@ -8,3 +8,9 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// API routes
+app.use('/api', apiRoutes);
